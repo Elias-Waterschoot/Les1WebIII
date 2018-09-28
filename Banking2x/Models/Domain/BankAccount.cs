311949ff51f76214d60a9bc24a8fdde55a0902cd
Elias <elias.waterschoot.y1187@student.hogent.be>
2018-09-28 12:21:31 +0200

    Create class BankAccount

---
+++ Banking2x/Models/Domain/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Banking2x.Models.Domain
+{
+    public class BankAccount
+    {
+        //private string _accountNumber;
+        //public const decimal WithdrawCost = 0.10M; //constante met hoofdletter (en is sws static)
+
+        #region Fields
+        public static readonly decimal _withdrawCost = 0.10M; //readonly declaren hier of in constructor (indien static altijd hier)
+
+        private decimal _balance;
+        #endregion
+
+        #region Properties
+        public decimal Balance
+        {
+            get
+
+            {
+                return _balance;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Balance cannot be negative");
+                _balance = value;
+            }
+        }
+
+        public string AccountNumber { get; private set; }
+        #endregion
+
+        #region Constructors
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount/*, int nrOfTimes = 1*/) //eerst verplichte parameters, dan optionele(die default waarde hebben
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Amount cannot be negative");
+            }
+            Balance += amount /** nrOfTimes*/;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        } 
+        #endregion
+
+
+    }
+}
--- Banking2x/Program.cs
+++ Banking2x/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking2x.Models.Domain;
+using System;
 
 namespace Banking2x
 {
@@ -6,7 +7,22 @@ namespace Banking2x
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myBA = new BankAccount("123-12312312-99");
+            Console.WriteLine($"Account {myBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.WriteLine($"Withdrawcost is {BankAccount._withdrawCost} Euro");
+
+            BankAccount myOtherBA = new BankAccount("123-12312312-99",50);
+            Console.WriteLine($"Other Account {myOtherBA.AccountNumber} created...");
+            Console.WriteLine($"Balance is currently {myOtherBA.Balance} Euro");
+
+            myBA.Deposit(1000);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(100);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.ReadLine();
         }
     }
 }
